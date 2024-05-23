cb0b5249bda231034aa21f6fda202d635b091c0e
Hampusholmberg <117671157+Hampusholmberg@users.noreply.github.com>
2024-05-23 11:44:52 +0200

    RUD func. implemented for website users

---
+++ Backoffice/Backoffice/Components/Models/WebsiteUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backoffice.Components.Models
+{
+    public class WebsiteUser
+    {
+        public string Id { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Biography { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? ProfileImage { get; set; } = "/images/icons/no-profile-picture.svg";
+        public int? AddressId { get; set; }
+        public WebsiteUserAddressModel? Address { get; set; }
+    }
+
+    public class WebsiteUserAddressModel
+    {
+        public string AddressLine1 { get; set; } = null!;
+        public string? AddressLine2 { get; set; }
+        public string PostalCode { get; set; } = null!;
+        public string City { get; set; } = null!;
+    }
+}
