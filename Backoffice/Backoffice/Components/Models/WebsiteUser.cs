c3da5f7dcaa43a839a5e25f882d52d6ec3bf914e
Hampusholmberg <117671157+Hampusholmberg@users.noreply.github.com>
2024-05-27 09:35:43 +0200

    newsletter checkbox implemented for website users update page

--- Backoffice/Backoffice/Components/Models/WebsiteUser.cs
+++ Backoffice/Backoffice/Components/Models/WebsiteUser.cs
@@ -8,5 +8,7 @@ public class WebsiteUser
     public string Email { get; set; } = null!;
     public string? PhoneNumber { get; set; }
     public string? Biography { get; set; }
+    public bool NewsletterSubscriber { get; set; }
+
     public WebsiteUserAddressModel Address { get; set; }
 }
